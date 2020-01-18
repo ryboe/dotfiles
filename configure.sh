e1d7f75222c9537e9d63c9ea6f1e313e5621cb28
Ryan Boehning <ryanboehning@gmail.com>
2020-01-17 21:10:08 -0800

    Update for changes in Jan 2020. (#2)
    Remove sublime configs.
    Add VS Code settings.json.
    Update .zshrc.
    Improve script that installs configs.

--- configure.sh
+++ configure.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/zsh
 
 set -euo pipefail
 
@@ -17,15 +17,17 @@ if [[ ! -x "$(command -v brew)" ]]; then
 fi
 
 # install brewed utils
-brew install bash curl exa fd fzf go git git-lfs python ripgrep shellcheck shfmt tree zsh
+brew install bash curl exa fd fzf go git git-lfs htop jq python ripgrep shellcheck shfmt tree zsh
 
 # load shell config
 curl --retry 3 --retry-delay 0 --retry-max-time 30 -sL https://raw.githubusercontent.com/y0ssar1an/dotfiles/master/.zshrc >"$HOME/.zshrc"
+
+# shellcheck source=/Users/ryan/.zshrc
 source ~/.zshrc
 
 # install rust
 if [[ ! -x "$(command -v rustup)" ]]; then
-	curl https://sh.rustup.rs -sSf | sh
+	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 	rustup toolchain install stable
 fi
 
@@ -35,37 +37,21 @@ curl --retry 3 --retry-delay 0 --retry-max-time 30 -sL https://raw.githubusercon
 curl --retry 3 --retry-delay 0 --retry-max-time 30 --create-dirs -sL https://raw.githubusercontent.com/y0ssar1an/dotfiles/master/.ssh/config >"$HOME/.ssh/config"
 curl --retry 3 --retry-delay 0 --retry-max-time 30 -sL https://raw.githubusercontent.com/y0ssar1an/dotfiles/master/.CFUserTextEncoding >"$HOME/.CFUserTextEncoding"
 
-# fetch sublime configs
-curl --retry 3 --retry-delay 0 --retry-max-time 30 --create-dirs -sL https://raw.githubusercontent.com/y0ssar1an/dotfiles/master/sublime/errfat.sublime-snippet >"$HOME/Library/Application Support/Sublime Text 3/Packages/User/errfat.sublime-snippet"
-curl --retry 3 --retry-delay 0 --retry-max-time 30 -sL https://raw.githubusercontent.com/y0ssar1an/dotfiles/master/sublime/errfatf.sublime-snippet >"$HOME/Library/Application Support/Sublime Text 3/Packages/User/errfatf.sublime-snippet"
-curl --retry 3 --retry-delay 0 --retry-max-time 30 -sL https://raw.githubusercontent.com/y0ssar1an/dotfiles/master/sublime/errnil.sublime-snippet >"$HOME/Library/Application Support/Sublime Text 3/Packages/User/errnil.sublime-snippet"
-curl --retry 3 --retry-delay 0 --retry-max-time 30 -sL https://raw.githubusercontent.com/y0ssar1an/dotfiles/master/sublime/fclose.sublime-snippet >"$HOME/Library/Application Support/Sublime Text 3/Packages/User/fclose.sublime-snippet"
-curl --retry 3 --retry-delay 0 --retry-max-time 30 -sL https://raw.githubusercontent.com/y0ssar1an/dotfiles/master/sublime/qq.sublime-snippet >"$HOME/Library/Application Support/Sublime Text 3/Packages/User/qq.sublime-snippet"
-curl --retry 3 --retry-delay 0 --retry-max-time 30 -sL https://raw.githubusercontent.com/y0ssar1an/dotfiles/master/sublime/Preferences.sublime-settings >"$HOME/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings"
-
 # install CLI utils
-mkdir ~/{bin,go,rs}
-cd rs
-git clone https://github.com/y0ssar1an/gitprompt
-git clone https://github.com/y0ssar1an/update-shell-utils
-cd gitprompt
-cargo install
-cd ../update-shell-utils
-cargo install
-cd
+mkdir ~/{bin,rs}
+cargo install --git https://github.com/y0ssar1an/gitprompt
+cargo install --git https://github.com/y0ssar1an/update-shell-utils
 
 echo '
 now install these:
-  amphetamine
   appcleaner
-  chrome
-  docker for mac
-  dropbox
+  code
+  etcher
+  firefox
   IINA
-  image optim
+  imageoptim
   iterm2
-  spectacle
-  sublime text
+  rectangle
   the unarchiver
   qbittorrent
 '
